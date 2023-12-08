0cdf7e6132b56316d7610732b71a3532d1648ab0
GreeNHaNd67 <144733532+GreeNHaNd67@users.noreply.github.com>
2023-12-07 17:40:29 -0800

    Delete Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/DAO/AdminDAO.java

--- Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/DAO/AdminDAO.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.mgen.bloodpressuremonitor.DAO;
-
-import edu.neu.mgen.bloodpressuremonitor.Entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminDAO extends JpaRepository<Admin,Integer> {
-}
