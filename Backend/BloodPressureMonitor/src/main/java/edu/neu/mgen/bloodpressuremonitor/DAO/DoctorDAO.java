a8613429aa6c5221c1706b58837e2daa45ec9220
GreeNHaNd67 <144733532+GreeNHaNd67@users.noreply.github.com>
2023-12-07 17:40:35 -0800

    Delete Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/DAO/DoctorDAO.java

--- Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/DAO/DoctorDAO.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.mgen.bloodpressuremonitor.DAO;
-
-import edu.neu.mgen.bloodpressuremonitor.Entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorDAO extends JpaRepository<Doctor,Integer> {
-}
