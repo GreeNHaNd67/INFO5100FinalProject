2b7203c74bf5d7d1a2d0b20826d2a70d77e48a17
GreeNHaNd67 <144733532+GreeNHaNd67@users.noreply.github.com>
2023-12-07 17:39:15 -0800

    Update AdminController.java

--- Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/Controllor/AdminController.java
+++ Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/Controllor/AdminController.java
@@ -1,9 +1,1 @@
-package edu.neu.mgen.bloodpressuremonitor.Controllor;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path="/admin")
-public class AdminController {
-}
