6eeebcd79ce23d750d7c0b20eeb1b1b3d6e89e2a
GreeNHaNd67 <144733532+GreeNHaNd67@users.noreply.github.com>
2023-12-07 17:40:12 -0800

    Delete Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/Controllor/DoctorController.java

--- Backend/BloodPressureMonitor/src/main/java/edu/neu/mgen/bloodpressuremonitor/Controllor/DoctorController.java
+++
@@ -1,9 +0,0 @@
-package edu.neu.mgen.bloodpressuremonitor.Controllor;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path="/doctor")
-public class DoctorController {
-}
